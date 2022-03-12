63113a949f6d970061a3c2649ddbb900211d45f0
phamhue-97 <phamhue97@gmail.com>
2022-03-12 13:28:24 +0700

    get price

--- frontend/src/services/index.tsx
+++ frontend/src/services/index.tsx
@@ -1,16 +1,58 @@
-import axios from "axios";
+import * as nearAPI from "near-api-js";
+import { tokenFomat } from "../utils/token";
 
-export const getUsdtOfToken = async () => {
-  const result = await axios
-    .get(
-      "https://api.coingecko.com/api/v3/simple/price?ids=ref-finance%2Cnear%2Cneo%2Caurora%2Cethereum%2Cxdai&vs_currencies=usd"
-    )
-    .then((response) => {
-      return response.data;
-    })
-    .catch((error) => {
-      return null
-    });
+const { connect, WalletConnection, keyStores } = nearAPI;
+const keyStore = new keyStores.BrowserLocalStorageKeyStore();
 
-  return result;
+const contractName = 'priceoracle.testnet';
+const nearConfig = {
+  networkId: "testnet",
+  nodeUrl: "https://rpc.testnet.near.org",
+  contractName,
+  walletUrl: "https://wallet.testnet.near.org",
+  helperUrl: "https://helper.testnet.near.org",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+export const _price = async function () {
+  return await connect({
+    deps: {
+      keyStore,
+    },
+    ...nearConfig,
+  });
+};
+
+export const _priceConnection = function (_price: any) {
+  return new WalletConnection(_price, null);
+};
+
+export const _contractPrice = function (wallet: any) {
+  return new nearAPI.Contract(wallet.account(), contractName, {
+    viewMethods: [
+      "get_price_data"
+    ],
+    changeMethods: [],
+  });
+};
+
+export const getUsdtOfToken = async function () {
+  const initPrice = await _price();
+  const initPriceConnect = _priceConnection(initPrice);
+  const initPriceContract: any = _contractPrice(initPriceConnect);
+  var asset_ids = Object.keys(tokenFomat).map(function(key) {
+    return key;
+  });
+  var prices = await initPriceContract.get_price_data({"asset_ids": asset_ids});
+  prices = prices.prices;
+  var results: any = {};
+  for (var i = 0; i < prices.length; i++) {
+    var element = prices[i];
+    var price = element.price ? element.price.multiplier / Math.pow(10, 4) : 1;
+    var token = tokenFomat[element.asset_id];
+    results[token.name] = {usd: price};
+  }
+  console.log('results: ', results);
+  return results;
 };
